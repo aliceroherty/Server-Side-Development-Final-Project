a4cbe9d6ececb12852a71cdd0a8e107b1e222041
Alice Roherty-Carrier <aliceroherty@gmail.com>
2019-10-08 13:43:54 -0300

    Inital Commit

---
+++ Website/Real Estate Application/Controllers/CreateAgentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Real_Estate_Application.Controllers
+{
+    public class CreateAgentController : Controller
+    {
+        // GET: CreateAgent
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
