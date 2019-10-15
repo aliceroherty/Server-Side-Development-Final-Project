8156c892be2bc05856ec948d6936e00acd30fb43
Alice Roherty-Carrier <aliceroherty@gmail.com>
2019-10-15 16:57:21 -0300

    Started View Listing HTML & CSS

---
+++ Website/Real Estate Application/Controllers/ViewListingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Real_Estate_Application.Controllers
+{
+    public class ViewListingController : Controller
+    {
+        // GET: ViewListing
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
